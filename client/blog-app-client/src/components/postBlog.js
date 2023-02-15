8aa1c3005ea51cc735f290e1784cf9f92212be8d
vineetha <yenugulavineetha2001@gmail.com>
2023-02-15 11:41:05 +0530

    added navigation to home

--- client/blog-app-client/src/components/postBlog.js
+++ client/blog-app-client/src/components/postBlog.js
@@ -2,31 +2,46 @@ import { useState} from 'react'
 import axios from 'axios'
 import { postBlogs } from './apiUtils'
 import { Nav } from './Nav'
+import { useNavigate } from 'react-router-dom'
 export const PostBlog = () => {
     const [title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
     const [Image, setImage] = useState("")
+    const [isloading,setIsloading]=useState(false)
+    const navigate=useNavigate()
     const submitPost = () => {
         if (Image) {
             postBlogs(title, Image, Description)
-                .then(res => console.log(res)).catch(err => console.log(err))
+                .then(res => {
+                    console.log(res)
+                  
+                }).catch(err => console.log(err))
         }
+        else{
+            alert("please select image..")
+        }
+        navigate('/')
     }
     const imageHandler = (img) => {
+        setIsloading(true)
         const formdata = new FormData()
         formdata.append("file", img)
         formdata.append("upload_preset", "blogs-app")
         axios.post("https://api.cloudinary.com/v1_1/dhtnrmzms/upload", formdata)
-            .then(res => setImage(res.data.url))
+            .then(res => {
+                setImage(res.data.url)
+                setIsloading(false)
+            })
             .catch(err => console.log(err))
     }
-    console.log(Image)
+    //console.log(Image)
     return (
         <div className='details'>
             <Nav />
             <h1 style={{ textAlign: "center", color: "white" }}>CREATE NEW POST</h1>
             <div className="post-create">
                 <form className='createpost'>
+                    {isloading?<div>please wait image is uploading....</div>:""}
                     <div className='cols'>
                         <label>Title</label>
                         <input type="text" placeholder="post title" value={title} onChange={(e) => setTitle(e.target.value)} />
